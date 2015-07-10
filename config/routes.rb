12655dc1618aa2e686b51242593c88e0fd7e67b8
Eric Park <erichoonpark@gmail.com>
2015-07-10 12:12:14 -0700

    API Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  namespace :api, defaults: { format: :json } do
+    match 'create_event', to: 'events#create', via: [:options]
+    resources :events, only: [:create]
+  end
   resources :apps
   devise_for :users
   get 'welcome/index'
