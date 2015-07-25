3369a89a427117a241a69f52e4ec5a6a56de4659
Eric Park <erichoonpark@gmail.com>
2015-07-25 16:26:08 -0700

    Events Controller

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -6,29 +6,30 @@ class API::EventsController < ApplicationController
     headers['Access-Control-Allow-Origin'] = '*'
     # Permit the POST,GET, OPTION request methods
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    # Allow the header Content-Type that declare the data type being sent 
+    # Allow the header Content-Type that declare the data type being sent
     headers['Access-Control-Allow-Headers'] = 'Content-Type'
   end
+
   skip_before_action :verify_authenticity_token
    # necessary in all controllers that will respond with JSON
   respond_to :json
 
   def create
     registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
-    if (registered_application == nil) {
+    if registered_application == nil
       render json: "Unregistered application", status: :unprocessable_entity
-    } else {
+    else
       render json: @event, status: :created
-    }
+    end
   end
 
   private
   # Error responses and before_action blocking work differently with Javascript requests.
   # Rather than using before_actions to authenticate actions, we suggest using
   # "guard clauses" like `permission_denied_error unless condition`
-  def event_params
-     params.permit(:event_name)
-  end
+    def event_params
+       params.permit(:event_name)
+    end
 
   # def permission_denied_error
   #   error(403, 'Permission Denied!')
@@ -42,6 +43,4 @@ class API::EventsController < ApplicationController
   #
   # render json: response.to_json, status: status
   # end
-
-  end
 end
